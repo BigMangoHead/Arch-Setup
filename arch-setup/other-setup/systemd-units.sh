6d8ca6a4af019bbdbb7f71037b32903bd64fc2c6
Isaac Mango <bigmangohead@gmail.com>
2024-11-27 22:53:53 -0500

    Add audio systemd unit

--- arch-setup/other-setup/systemd-units.sh
+++ arch-setup/other-setup/systemd-units.sh
@@ -2,3 +2,7 @@
 
 systemctl --user enable --now ssh-agent # Authentification for github
 sudo systemctl enable --now udevmon # Remaps caps lock
+
+# Enable audio
+systemctl --user enable pipewire 
+systemctl --user enable pipewire-pulse
