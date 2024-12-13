befa83b79be118c9985d2f89ce4dd2ec397b89a0
Isaac Mango <bigmangohead@gmail.com>
2024-12-12 20:07:41 -0500

    Automatically sync math files

--- arch-setup/other-setup/systemd-units.sh
+++ arch-setup/other-setup/systemd-units.sh
@@ -2,6 +2,7 @@
 
 systemctl --user enable --now ssh-agent # Authentification for github
 sudo systemctl enable --now udevmon # Remaps caps lock
+systemctl enable --now --user sync-math.timer # Enable math file syncing
 
 # Enable audio
 systemctl --user enable pipewire 
