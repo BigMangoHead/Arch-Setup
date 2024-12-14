0602f862345b3717b1e012c448be553fbbf13927
Isaac Mango <bigmangohead@gmail.com>
2024-12-14 14:22:52 -0500

    Periodic file syncing with phone

---
+++ arch-setup/other-setup/misc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Make directories needed for phone file syncing
+mkdir /media
+mkdir /media/phone
--- arch-setup/other-setup/systemd-units.sh
+++ arch-setup/other-setup/systemd-units.sh
@@ -3,6 +3,7 @@
 systemctl --user enable --now ssh-agent # Authentification for github
 sudo systemctl enable --now udevmon # Remaps caps lock
 systemctl enable --now --user sync-math.timer # Enable math file syncing
+sudo systemctl enable --now sync-phone.timer # Enable phone file syncing
 
 # Enable audio
 systemctl --user enable pipewire 
