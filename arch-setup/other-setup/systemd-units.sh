fe55c6d63f9417d60a03b0293f64a59141c67ea5
Isaac Mango <bigmangohead@gmail.com>
2025-01-14 09:26:06 -0500

    Add todoist

--- arch-setup/other-setup/systemd-units.sh
+++ arch-setup/other-setup/systemd-units.sh
@@ -5,6 +5,7 @@ sudo systemctl enable --now udevmon # Remaps caps lock
 systemctl enable --now --user sync-math.timer # Enable math file syncing
 sudo systemctl enable --now sync-phone.timer # Enable phone file syncing
 systemctl enable --now --user syncthing.service # File syncing through syncthing
+sudo systemctl enable --now snapd.service # Downloading other applications
 
 # Enable audio
 systemctl --user enable pipewire 
