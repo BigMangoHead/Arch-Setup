1370bfb8230a053ee0937baa5d1d39d1e419309b
Isaac Mango <bigmangohead@gmail.com>
2025-02-06 22:50:25 -0500

    Remove sync-math w/ rclone, change window config, add package dep

--- arch-setup/other-setup/systemd-units.sh
+++ arch-setup/other-setup/systemd-units.sh
@@ -2,7 +2,6 @@
 
 systemctl --user enable --now ssh-agent # Authentification for github
 sudo systemctl enable --now udevmon # Remaps caps lock
-systemctl enable --now --user sync-math.timer # Enable math file syncing
 sudo systemctl enable --now sync-phone.timer # Enable phone file syncing
 systemctl enable --now --user syncthing.service # File syncing through syncthing
 sudo systemctl enable --now snapd.service # Downloading other applications
