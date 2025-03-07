96856d8a96f6e0595a0d8511454592ffd5f9bf90
Isaac Mango <bigmangohead@gmail.com>
2025-03-06 23:18:07 -0500

    Add lua packages for grade-reader

--- arch-setup/other-setup/lua.sh
+++ arch-setup/other-setup/lua.sh
@@ -2,5 +2,7 @@
 
 sudo luarocks install luafilesystem
 sudo luarocks install lunajson
+sudo luarocks install csv
+sudo luarocks install eansi
 
 mkdir -p ~/scripts/data # Needed to store data for mn project
