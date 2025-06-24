109c429e06e6d3737ff86f2eb3566c2f49605102
Isaac Mango <bigmangohead@gmail.com>
2025-06-23 23:48:25 -0500

    Enable timezone ntpd systemd unit

--- arch-setup/other-setup/systemd-units.sh
+++ arch-setup/other-setup/systemd-units.sh
@@ -5,6 +5,7 @@ sudo systemctl enable --now udevmon # Remaps caps lock
 sudo systemctl enable --now sync-phone.timer # Enable phone file syncing
 systemctl enable --now --user syncthing.service # File syncing through syncthing
 sudo systemctl enable --now snapd.service # Downloading other applications
+sudo systemctl enable --now ntpd.service # Time synchronization
 
 # Enable audio
 systemctl --user enable pipewire 
