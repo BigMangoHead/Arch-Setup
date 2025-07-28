aa50d6804cb87acbc4772d8ac3d54bc9f1b11942
Isaac Mango <bigmangohead@gmail.com>
2025-07-27 19:24:40 -0500

    Large improvements to latex setup

--- .config/nvim/LuaSnip/tex/init.lua
+++ .config/nvim/LuaSnip/tex/init.lua
@@ -4,109 +4,25 @@ local line_begin = helper.line_begin
 local in_math = helper.in_math
 local in_text = helper.in_text
 
+local function environment(trig, env, dscr)
+    return s({ trig = trig, dscr = dscr },
+        fmta(
+            [[
+                \begin{]] .. env .. [[}
+                    <>
+                \end{]] .. env .. [[}
+                <>
+            ]],
+            { i(1), i(0) }
+        ),
+        {condition = line_begin}
+    )
+end
+
 -- Snippets
 return {
     -- Jump forward
     -- s({ trig = "; ", snippetType = "autosnippet", dscr = "Jump forward in math mode" },
-
-
-    -- Equation
-    s({ trig = "eq", dscr = "Expands into equation environment" },
-        fmta(
-            [[
-                \begin{equation*}
-                    <>
-                \end{equation*}
-                <>
-            ]],
-            { i(1), i(0) }
-        ),
-        {condition = line_begin}
-    ),
-
-    -- Align
-    s({ trig = "al", dscr = "Expands into align environment" },
-        fmta(
-            [[
-                \begin{align*}
-                    <>
-                \end{align*}
-                <>
-            ]],
-            { i(1), i(0) }
-        ),
-        {condition = line_begin}
-    ),
-
-    -- Proof environment
-    s({ trig = "pf", dscr = "Expands into proof environment" },
-        fmta(
-            [[
-                \begin{proof}
-                    <>
-                \end{proof}
-                <>
-            ]],
-            { i(1), i(0) }
-        ),
-        {condition = line_begin}
-    ),
-
-    -- Theorem environment
-    s({ trig = "th", dscr = "Expands into theorem environment" },
-        fmta(
-            [[
-                \begin{theorem}
-                    <>
-                \end{theorem}
-                <>
-            ]],
-            { i(1), i(0) }
-        ),
-        {condition = line_begin}
-    ),
-
-    -- Lemma environment
-    s({ trig = "le", dscr = "Expands into lemma environment" },
-        fmta(
-            [[
-                \begin{lemma}
-                    <>
-                \end{lemma}
-                <>
-            ]],
-            { i(1), i(0) }
-        ),
-        {condition = line_begin}
-    ),
-
-    -- Corollary environment
-    s({ trig = "co", dscr = "Expands into corollary environment" },
-        fmta(
-            [[
-                \begin{corollary}
-                    <>
-                \end{corollary}
-                <>
-            ]],
-            { i(1), i(0) }
-        ),
-        {condition = line_begin}
-    ),
-
-    -- Definition environment
-    s({ trig = "de", dscr = "Expands into definition environment" },
-        fmta(
-            [[
-                \begin{definition}
-                    <>
-                \end{definition}
-                <>
-            ]],
-            { i(1), i(0) }
-        ),
-        {condition = line_begin}
-    ),
 
     -- Begin environment
     s({ trig = "be", dscr = "Expands into beginning any environment" },
@@ -134,11 +50,70 @@ return {
     s({ trig = "se", snippetType = "autosnippet" , dscr = "Expands into inline math mode"},
         fmta(
             [[
-                \section*{<>}
-                <>
+                \section{<>}
             ]],
-            { i(1), i(0) }
+            { i(1) }
         ),
         {condition = line_begin}
     ),
+
+    -- Spam of different environments I use
+    -- Equation
+    environment("eq", "equation*", "Expands into equation environment"),
+    environment("equ", "equation*", "Expands into equation environment"),
+
+    -- Align
+    environment("al", "align*", "Expands into align environment"),
+    environment("ali", "align*", "Expands into align environment"),
+
+    -- Proof environment
+    environment("pr", "proof", "Expands into proof environment"),
+    environment("pf", "proof", "Expands into proof environment"),
+    environment("pro", "proof", "Expands into proof environment"),
+
+    -- Theorem environment
+    environment("th", "theorem", "Expands into theorem environment"),
+    environment("the", "theorem", "Expands into theorem environment"),
+
+    -- Lemma environment
+    environment("le", "lemma", "Expands into lemma environment"),
+    environment("lem", "lemma", "Expands into lemma environment"),
+
+    -- Corollary environment
+    environment("cor", "corollary", "Expands into corollary environment"),
+
+    -- Conjecture environment
+    environment("con", "conjecture", "Expands into conjecture environment"),
+
+    -- Definition environment
+    environment("de", "definition", "Expands into definition environment"),
+    environment("def", "definition", "Expands into definition environment"),
+
+    -- Claim environment
+    environment("cl", "claim", "Expands into claim environment"),
+    environment("cla", "claim", "Expands into claim environment"),
+
+    -- Note environment
+    environment("no", "note", "Expands into note environment"),
+    environment("not", "note", "Expands into note environment"),
+
+    -- Remark environment
+    environment("re", "remark", "Expands into remark environment"),
+    environment("rem", "remark", "Expands into remark environment"),
+
+    -- Fact environment
+    environment("fa", "fact", "Expands into fact environment"),
+    environment("fac", "fact", "Expands into fact environment"),
+
+    -- Exercise environment
+    environment("ex", "exercise", "Expands into exercise environment"),
+    environment("exe", "exercise", "Expands into exercise environment"),
+    environment("ex*", "exercise*", "Expands into exercise* environment"),
+
+    -- Problem environment
+    environment("pb", "problem", "Expands into problem environment"),
+    
+    -- Enumerate environment
+    environment("en", "enumerate", "Expands into enumerate environment"),
+    environment("enu", "enumerate", "Expands into enumerate environment"),
 }
