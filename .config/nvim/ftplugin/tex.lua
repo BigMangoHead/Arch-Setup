65ea66ea333eeb4784f51129a1db77c075b3cc39
Isaac Mango <bigmangohead@gmail.com>
2025-08-30 16:41:43 -0400

    LaTeX setup improvements

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -10,6 +10,7 @@ vim.g.vimtex_compiler_latexmk = {
 -- Keybinds
 -- Compile command
 vim.keymap.set({'n', 's', 'v', 'o'}, "<localleader>c", "<Cmd>write<CR><Cmd>VimtexCompile<CR>")
+vim.keymap.set({'n', 's', 'v', 'o'}, "<localleader>j", "<Cmd>write<CR>")
 
 -- Autocompile command
 vim.keymap.set({'n', 's', 'v', 'o'}, "<localleader>C", "<Cmd>ToggleAutosave<CR><Cmd>VimtexCompile<CR>")
