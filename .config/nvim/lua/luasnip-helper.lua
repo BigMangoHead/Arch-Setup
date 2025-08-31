4088d500100bf689db80a71793b8159810b1d17b
Isaac Mango <bigmangohead@gmail.com>
2025-08-31 18:28:10 -0400

    Snippet improvements

--- .config/nvim/lua/luasnip-helper.lua
+++ .config/nvim/lua/luasnip-helper.lua
@@ -3,6 +3,7 @@ local M = {}
 local ls = require("luasnip")
 local sn = ls.snippet_node
 local i = ls.insert_node
+local t = ls.text_node
 
 -- Only at start of line
 M.line_begin = require("luasnip.extras.expand_conditions").line_begin
@@ -15,6 +16,14 @@ end
 -- Only in text mode
 function M.in_text()
     return not M.in_math()
+end
+
+function M.get_visual(args, parent)
+  if (#parent.snippet.env.LS_SELECT_RAW > 0) then
+    return sn(nil, t(parent.snippet.env.LS_SELECT_RAW))
+  else  -- If LS_SELECT_RAW is empty, return a blank insert node
+    return sn(nil, i(1))
+  end
 end
 
 return M
