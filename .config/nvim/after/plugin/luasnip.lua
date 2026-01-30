722ba4a1073890dfe937078d1dce8adc192c70bd
Isaac Mango <bigmangohead@gmail.com>
2026-01-30 01:24:43 -0500

    Snippet and keybind improvements

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -1,7 +1,8 @@
 local ls = require("luasnip")
 
-vim.keymap.set({"i"}, "<Tab>", function() ls.expand_or_jump() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<Tab>", function() ls.expand_or_jump() end, {silent = true})
 vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump(-1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-f>", "<Plug>luasnip-next-choice")
 
 -- Load snippets from ~/.config/nvim/LuaSnip/
 require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/LuaSnip/"})
