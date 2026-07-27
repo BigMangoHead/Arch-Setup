814344398c6d9cf6956ffa500893619249968622
Isaac Mango <bigmangohead@gmail.com>
2026-07-27 17:47:07 -0500

    various minor updates

--- arch-setup/other-setup/lua.sh
+++ arch-setup/other-setup/lua.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo luarocks install luafilesystem
-sudo luarocks install lunajson
-sudo luarocks install csv
-sudo luarocks install eansi
+sudo luarocks install luafilesystem # file system handling
+sudo luarocks install lunajson # json handling
+sudo luarocks install csv # CSV file handling
+sudo luarocks install eansi # ANSI color handling
+sudo luarocks install argparse # cmdline arg parsing
+sudo luarocks install lua-dotenv # .env file reader
 
 mkdir -p ~/scripts/data # Needed to store data for mn project
